36e688900fd4121b5946ae7772befc4dd9820dcb
yangchong211 <yangchong211@163.com>
2019-03-11 14:52:24 +0800

    更新wiki

--- app/src/main/java/org/yczbj/ycvideoplayer/ui/person/TestFirstVideoActivity.java
+++ app/src/main/java/org/yczbj/ycvideoplayer/ui/person/TestFirstVideoActivity.java
@@ -28,6 +28,7 @@ public class TestFirstVideoActivity extends BaseActivity  {
 
     @Bind(R.id.nice_video_player)
     VideoPlayer videoPlayer;
+    private VideoPlayerController controller;
 
 
     @Override
@@ -70,7 +71,7 @@ public class TestFirstVideoActivity extends BaseActivity  {
         //必须关键的4步，播放视频最简单的方式
         videoPlayer.setPlayerType(ConstantKeys.IjkPlayerType.TYPE_NATIVE);
         videoPlayer.setUp(ConstantVideo.VideoPlayerList[0], null);
-        VideoPlayerController controller = new VideoPlayerController(this);
+        controller = new VideoPlayerController(this);
         videoPlayer.setController(controller);
     }
 
@@ -82,6 +83,89 @@ public class TestFirstVideoActivity extends BaseActivity  {
     @Override
     public void initData() {
 
+    }
+
+    private void test(){
+        //关于视频播放相关api
+        //获取缓冲区百分比
+        int bufferPercentage = videoPlayer.getBufferPercentage();
+        //获取播放位置
+        long currentPosition = videoPlayer.getCurrentPosition();
+        //获取当前播放模式
+        int currentState = videoPlayer.getCurrentState();
+        //获取持续时长
+        long duration = videoPlayer.getDuration();
+        //获取最大音量
+        int maxVolume = videoPlayer.getMaxVolume();
+        //获取当前播放状态
+        int playType = videoPlayer.getPlayType();
+        //获取播放速度
+        long tcpSpeed = videoPlayer.getTcpSpeed();
+        //获取音量值
+        int volume = videoPlayer.getVolume();
+
+
+        //判断是否是否缓冲暂停
+        boolean bufferingPaused = videoPlayer.isBufferingPaused();
+        //判断视频是否正在缓冲(播放器正在播放时，缓冲区数据不足，进行缓冲，缓冲区数据足够后恢复播放)
+        boolean bufferingPlaying = videoPlayer.isBufferingPlaying();
+        //判断视频是否播放完成
+        boolean completed = videoPlayer.isCompleted();
+        //判断视频是否播放错误
+        boolean error = videoPlayer.isError();
+        //判断视频是否播放全屏
+        boolean fullScreen = videoPlayer.isFullScreen();
+        //判断是否开始播放
+        boolean idle = videoPlayer.isIdle();
+        //判断视频是否正常播放
+        boolean normal = videoPlayer.isNormal();
+        //判断视频是否暂停播放
+        boolean paused = videoPlayer.isPaused();
+        //判断视频是否正在播放
+        boolean playing = videoPlayer.isPlaying();
+        //判断视频是否准备就绪
+        boolean prepared = videoPlayer.isPrepared();
+        //判断视频是否播放准备中
+        boolean preparing = videoPlayer.isPreparing();
+        //判断视频是否播放小窗口
+        boolean tinyWindow = videoPlayer.isTinyWindow();
+
+        //进入全屏模式
+        videoPlayer.enterFullScreen();
+        //进入竖屏的全屏模式
+        videoPlayer.enterVerticalScreenScreen();
+        //进入小窗口播放
+        //注意：小窗口播放视频比例是        16：9
+        videoPlayer.enterTinyWindow();
+
+        //释放，内部的播放器被释放掉，同时如果在全屏、小窗口模式下都会退出
+        videoPlayer.release();
+        //释放播放器，注意一定要判断对象是否为空，增强严谨性
+        videoPlayer.releasePlayer();
+
+        //设置播放器类型，必须设置
+        //输入值：ConstantKeys.IjkPlayerType.TYPE_IJK   或者  ConstantKeys.IjkPlayerType.TYPE_NATIVE
+        videoPlayer.setPlayerType(ConstantKeys.IjkPlayerType.TYPE_NATIVE);
+        //设置播放位置
+        videoPlayer.seekTo(100);
+        //设置播放速度，不必须
+        videoPlayer.setSpeed(100);
+        //设置视频链接
+        videoPlayer.setUp("",null);
+        //设置音量
+        videoPlayer.setVolume(50);
+
+        //是否从上一次的位置继续播放
+        videoPlayer.continueFromLastPosition(true);
+
+        //开始播放
+        videoPlayer.start();
+        //暂停播放
+        videoPlayer.pause();
+        //开始播放
+        videoPlayer.start(100);
+        //重新播放
+        videoPlayer.restart();
     }
 
 }
--- read/wiki1.md
+++ read/wiki1.md
@@ -259,19 +259,99 @@
 
 
 ### 11.常见api说明
+#### 11.1 关于VideoPlayer中设置属性方法
+- 如下所示
+    ```
+    //进入全屏模式
+    videoPlayer.enterFullScreen();
+    //进入竖屏的全屏模式
+    videoPlayer.enterVerticalScreenScreen();
+    //进入小窗口播放
+    //注意：小窗口播放视频比例是        16：9
+    videoPlayer.enterTinyWindow();
+
+    //释放，内部的播放器被释放掉，同时如果在全屏、小窗口模式下都会退出
+    videoPlayer.release();
+    //释放播放器，注意一定要判断对象是否为空，增强严谨性
+    videoPlayer.releasePlayer();
+
+    //设置播放器类型，必须设置
+    //输入值：ConstantKeys.IjkPlayerType.TYPE_IJK   或者  ConstantKeys.IjkPlayerType.TYPE_NATIVE
+    videoPlayer.setPlayerType(ConstantKeys.IjkPlayerType.TYPE_NATIVE);
+    //设置播放位置
+    videoPlayer.seekTo(100);
+    //设置播放速度，不必须
+    videoPlayer.setSpeed(100);
+    //设置视频链接
+    videoPlayer.setUp("",null);
+    //设置音量
+    videoPlayer.setVolume(50);
+
+    //是否从上一次的位置继续播放
+    videoPlayer.continueFromLastPosition(true);
+    ```
 
 
+#### 11.2 关于VideoPlayer中获取属性方法
+- 如下所示
+    ```
+    //关于视频播放相关api
+    //获取缓冲区百分比
+    int bufferPercentage = videoPlayer.getBufferPercentage();
+    //获取播放位置
+    long currentPosition = videoPlayer.getCurrentPosition();
+    //获取当前播放模式
+    int currentState = videoPlayer.getCurrentState();
+    //获取持续时长
+    long duration = videoPlayer.getDuration();
+    //获取最大音量
+    int maxVolume = videoPlayer.getMaxVolume();
+    //获取当前播放状态
+    int playType = videoPlayer.getPlayType();
+    //获取播放速度
+    long tcpSpeed = videoPlayer.getTcpSpeed();
+    //获取音量值
+    int volume = videoPlayer.getVolume();
+    ```
+
+#### 11.3 关于VideoPlayer中设置播放状态方法
+- 如下所示
+    ```
+    //判断是否是否缓冲暂停
+    boolean bufferingPaused = videoPlayer.isBufferingPaused();
+    //判断视频是否正在缓冲(播放器正在播放时，缓冲区数据不足，进行缓冲，缓冲区数据足够后恢复播放)
+    boolean bufferingPlaying = videoPlayer.isBufferingPlaying();
+    //判断视频是否播放完成
+    boolean completed = videoPlayer.isCompleted();
+    //判断视频是否播放错误
+    boolean error = videoPlayer.isError();
+    //判断视频是否播放全屏
+    boolean fullScreen = videoPlayer.isFullScreen();
+    //判断是否开始播放
+    boolean idle = videoPlayer.isIdle();
+    //判断视频是否正常播放
+    boolean normal = videoPlayer.isNormal();
+    //判断视频是否暂停播放
+    boolean paused = videoPlayer.isPaused();
+    //判断视频是否正在播放
+    boolean playing = videoPlayer.isPlaying();
+    //判断视频是否准备就绪
+    boolean prepared = videoPlayer.isPrepared();
+    //判断视频是否播放准备中
+    boolean preparing = videoPlayer.isPreparing();
+    //判断视频是否播放小窗口
+    boolean tinyWindow = videoPlayer.isTinyWindow();
 
 
-
-
-
-
-
-
-
-
-
+    //开始播放
+    videoPlayer.start();
+    //暂停播放
+    videoPlayer.pause();
+    //开始播放
+    videoPlayer.start(100);
+    //重新播放
+    videoPlayer.restart();
+    ```
 
 
 
