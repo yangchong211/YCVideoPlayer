b0db213baca39f16c8483bd8944c82222884f677
yangchong <yangchong211@163.com>
2021-04-26 16:09:24 +0800

    添加工具类

--- VideoTool/src/main/java/com/yc/videotool/Tool.java
+++ VideoTool/src/main/java/com/yc/videotool/Tool.java
@@ -1,9 +1,21 @@
 package com.yc.videotool;
 
+import android.util.Log;
+
 public final class Tool {
 
     /**
      * 工具基础库介绍
      */
+
+    private void test(){
+        boolean fastDoubleClick = ClickUtils.isFastDoubleClick();
+        //boolean fastDoubleClick = ClickUtils.isFastDoubleClick(800);
+        if (fastDoubleClick){
+            return;
+        }
+        //todo
+        Log.i("dianji","");
+    }
 
 }
